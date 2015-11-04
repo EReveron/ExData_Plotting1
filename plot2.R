53a5872400e86a4b197439ca34466233eb219707
EReveron <enrique.reveron@gmail.com>
2015-11-04 12:33:28 -0430

    Final version

--- plot2.R
+++ plot2.R
@@ -1,4 +1,18 @@
 plot2 <- function() {
+	## plot2.R
+	## Create Plot:  'Global_active_power' by 'time' requested by the Coursera Exploratory Data Analysis
+	## 		Course Project 1
+	## Written by: Enrique Reveron
+
+	## Set system locale to English to have the same output requested (strptime)
+	Sys.setlocale("LC_TIME", "English")
+
+	## Check if the data file is located in the working dir
+
+	if (!file.exists("household_power_consumption.txt"))
+	{
+		{ stop("no valid data file in working directory") }
+	}
 
 	## Read Only the Requested Data
 	## We must read the data from 1/2/2007 to 2/2/2007 (two days complete days)
@@ -19,36 +33,27 @@ plot2 <- function() {
 	rows_n <- as.numeric(difftime(y2,x2,units="mins")) + 1 ## is neccesary to add the first measure
 
 	
-
+	## Read the Information
 	dt <- read.delim("household_power_consumption.txt",header=TRUE,sep = ";",
 			skip= skip_n,nrows=rows_n, 
-			col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")) 
+			col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage",
+			"Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")) 
 
+	## Create File and Plot it
 
-	
-
-	## Format the values in the dataset
-
-	##dt[,1] <- as.Date(strptime(dt[,1],"%d/%m/%Y"))
-	##dt[,2] <- as.Date(strptime(dt[,2],"%H:%M:%S"))
-
-	dt[,10] <- as.Date(strptime(paste(dt[,1],dt[,2]),"%d/%m/%Y %H:%M:%S"))
-	dt[,11] <- weekdays.Date(dt[,10])
-	
-	##dt
-	## Create Histogram and Plot it
-
-	png(filename="plo2.png", 
+	png(filename="plot2.png", 
     		units="px", 
     		width=480, 
     		height=480, 
     		pointsize=12, 
     		res=72)
-	
-	##histogram <- hist(dt$Global_active_power, plot = FALSE)
- 	##plot(dt$Global_active_power,dt$V11,  type = "l", main = "", ylab = "Global Active Power (kilowatts)", xlab = "") 
-	
-	plot(y=dt$Global_active_power,x=dt$Time,  type = "l", main = "", ylab = "Global Active Power (kilowatts)", xlab = "")
 
+	## Create a row with the Date + Time values to be used as x axis for plot
+
+	x_time <- strptime(paste(dt[,1],dt[,2]),"%d/%m/%Y %H:%M:%S")	
+
+ 	plot(x = x_time, y = dt$Global_active_power,type = "l", main = "", 
+		ylab = "Global Active Power (kilowatts)", xlab = "") 
 	dev.off()
+
 }
